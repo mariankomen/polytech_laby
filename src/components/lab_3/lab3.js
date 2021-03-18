0df65678ac0567d683cd6a06c56f6ff7b151f288
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2021-03-18 17:06:10 +0200

    first commit

---
+++ src/components/lab_3/lab3.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Lab3 = () => {
+    return(
+        <div>
+            Lab2
+        </div>
+    )
+}
+
+export default Lab3
---
+++ src/components/lab_9/lab9.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Lab2 = () => {
+    return(
+        <div>
+            Lab2
+        </div>
+    )
+}
+
+export default Lab2
